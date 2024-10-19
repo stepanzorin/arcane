5a35775e33501418ae44499b5c350de31de648ed
Stepan Zorin <stz.hom@gmail.com>
2024-10-20 00:05:34 +0300

    created `app_config`

---
+++ src/app_config.cpp
@@ -0,0 +1,95 @@
+#include "app_config.hpp"
+
+#include <fstream>
+#include <stdexcept>
+#include <string_view>
+
+#include <boost/json.hpp>
+#include <boost/json/array.hpp>
+#include <boost/json/value.hpp>
+#include <boost/json/value_to.hpp>
+#include <fmt/format.h>
+
+#include "util/pretty_json.hpp"
+
+namespace sm::arcane {
+
+namespace detail {
+
+void validate_version(const version_levels_s config_project_version) {
+    static constexpr auto cmake_project_version = version_levels_s{SM_ARCANE_PROJECT_VERSION_MAJOR,
+                                                                   SM_ARCANE_PROJECT_VERSION_MINOR,
+                                                                   SM_ARCANE_PROJECT_VERSION_PATCH};
+
+    if (config_project_version != cmake_project_version) {
+        throw std::runtime_error(fmt::format("Config project version [{}, {}, {}] doesn't match with version indicated "
+                                             "in the root CMakeLists.txt [{}, {}, {}]",
+                                             config_project_version.major,
+                                             config_project_version.minor,
+                                             config_project_version.patch,
+                                             cmake_project_version.major,
+                                             cmake_project_version.minor,
+                                             cmake_project_version.patch));
+    }
+}
+
+} // namespace detail
+
+namespace json = boost::json;
+
+namespace {
+
+[[nodiscard]] std::string title_from_json(const json::value &desc) noexcept { return desc.as_string().c_str(); }
+[[nodiscard]] json::value title_to_json(const std::string_view title) { return title.data(); }
+
+[[nodiscard]] detail::version_levels_s collect_levels(const json::array &version_levels) noexcept {
+    const auto major = json::value_to<std::uint8_t>(version_levels[0]);
+    const auto minor = json::value_to<std::uint8_t>(version_levels[1]);
+    const auto patch = json::value_to<std::uint8_t>(version_levels[2]);
+    return {.major = major, .minor = minor, .patch = patch};
+}
+[[nodiscard]] detail::version_levels_s version_from_json(const json::value &desc) {
+    const auto version_levels = collect_levels(desc.as_array());
+    detail::validate_version(version_levels);
+
+    return version_levels;
+}
+[[nodiscard]] json::value version_to_json(const detail::version_levels_s version) {
+    return {version.major, version.minor, version.patch};
+}
+
+inline constexpr auto g_config_path = SM_ARCANE_APP_CONFIG_PATH;
+
+} // namespace
+
+[[nodiscard]] app_config_s app_config_from_json() {
+    auto file = std::ifstream{g_config_path};
+
+    if (!file.is_open()) {
+        throw std::runtime_error{"Filed to open the app config file for the reading"};
+    }
+
+    const auto json_content = std::string{(std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>()};
+    const auto json_value = json::parse(json_content);
+    const auto &json_desc = json_value.as_object();
+
+    const auto &app_desc = json_desc.at("app").as_object();
+    return {.title = title_from_json(app_desc.at("title")),
+            .version = version_from_json(app_desc.at("version")),
+            .window_config = window_config_from_json(app_desc.at("window"))};
+}
+void app_config_to_json(const app_config_s &config) {
+    auto file = std::fstream{g_config_path};
+    if (!file.is_open()) {
+        throw std::runtime_error{"Failed to open the app config file for the writing"};
+    }
+
+    const auto json_value = json::value{{"app",
+                                         {{"title", title_to_json(config.title)},
+                                          {"version", version_to_json(config.version)},
+                                          {"window", window_config_to_json(config.window_config)}}}};
+
+    util::write_pretty_json(file, json_value);
+}
+
+} // namespace sm::arcane
--- src/app_config.hpp
+++ src/app_config.hpp
@@ -4,21 +4,39 @@
 #pragma once
 
 #include <cstdint>
-#include <string_view>
+#include <string>
+
+#include <boost/describe/class.hpp>
+#include <boost/describe/enum.hpp>
+#include <boost/json/fwd.hpp>
+
+#include "window_config.hpp"
 
 namespace sm::arcane {
 
-struct app_config_s {
-private:
-    struct version_levels_s {
-        std::uint8_t major = SM_ARCANE_PROJECT_VERSION_MAJOR;
-        std::uint8_t minor = SM_ARCANE_PROJECT_VERSION_MINOR;
-        std::uint8_t patch = SM_ARCANE_PROJECT_VERSION_PATCH;
-    };
+namespace detail {
 
-public:
-    std::string_view title = SM_ARCANE_PROJECT_NAME;
-    version_levels_s version = {};
+struct version_levels_s {
+    std::uint8_t major = SM_ARCANE_PROJECT_VERSION_MAJOR;
+    std::uint8_t minor = SM_ARCANE_PROJECT_VERSION_MINOR;
+    std::uint8_t patch = SM_ARCANE_PROJECT_VERSION_PATCH;
+
+    [[nodiscard]] bool operator==(const version_levels_s &) const noexcept = default;
+
+    BOOST_DESCRIBE_STRUCT(version_levels_s, (), (major, minor, patch))
 };
+
+} // namespace detail
+
+struct app_config_s {
+    std::string title = SM_ARCANE_PROJECT_NAME;
+    detail::version_levels_s version;
+    window_config_s window_config;
+
+    BOOST_DESCRIBE_STRUCT(app_config_s, (), (title, version, window_config))
+};
+
+[[nodiscard]] app_config_s app_config_from_json();
+void app_config_to_json(const app_config_s & /* config */);
 
 } // namespace sm::arcane
