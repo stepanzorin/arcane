e4d6ee3a7e4170cdff63bcf41537fb0e9107d48b
Stepan Zorin <stz.hom@gmail.com>
2024-10-22 01:20:31 +0300

    changed `std::vector<std::string>` -> `std::vector<const char *>` in `vulkan_config`

--- src/vulkan_config.cpp
+++ src/vulkan_config.cpp
@@ -10,20 +10,44 @@ namespace json = boost::json;
 
 namespace {
 
+namespace detail {
+
+[[nodiscard]] std::vector<const char *> vector_string_to_vector_const_char(const std::vector<std::string> &strings) {
+    auto vector_cstr = std::vector<const char *>{};
+    vector_cstr.reserve(strings.size());
+    for (const auto &str : strings) {
+        vector_cstr.push_back(str.c_str());
+    }
+    return vector_cstr;
+}
+[[nodiscard]] std::vector<std::string> vector_const_char_to_vector_string(const std::vector<const char *> &cstrs) {
+    auto vector_string = std::vector<std::string>{};
+    vector_string.reserve(cstrs.size());
+    for (const auto *cstr : cstrs) {
+        vector_string.emplace_back(cstr);
+    }
+    return vector_string;
+}
+
+} // namespace detail
+
 [[nodiscard]] vulkan_layers_s vulkan_layers_from_json(const json::value &layers_desc) {
-    return {.monitor_layers = json::value_to<std::vector<std::string>>(layers_desc.at("monitor_layers")),
-            .validation_layers = json::value_to<std::vector<std::string>>(layers_desc.at("validation_layers"))};
+    const auto validation_layers = json::value_to<std::vector<std::string>>(layers_desc.at("validation_layers"));
+    const auto monitor_layers = json::value_to<std::vector<std::string>>(layers_desc.at("monitor_layers"));
+    return {.validation_layers = detail::vector_string_to_vector_const_char(validation_layers),
+            .monitor_layers = detail::vector_string_to_vector_const_char(monitor_layers)};
 }
 [[nodiscard]] json::value vulkan_layers_to_json(const vulkan_layers_s &layers) {
-    auto monitor_layers = json::array(layers.monitor_layers.begin(), layers.monitor_layers.end());
     auto validation_layers = json::array(layers.validation_layers.begin(), layers.validation_layers.end());
-    return {{"monitor_layers", std::move(monitor_layers)}, {"validation_layers", std::move(validation_layers)}};
+    auto monitor_layers = json::array(layers.monitor_layers.begin(), layers.monitor_layers.end());
+    return {{"validation_layers", std::move(validation_layers)}, {"monitor_layers", std::move(monitor_layers)}};
 }
 
 [[nodiscard]] device_config_s device_config_from_json(const json::value &device_desc) {
+    const auto extensions = json::value_to<std::vector<std::string>>(device_desc.at("extensions"));
     return {.enable_anisotropy = json::value_to<bool>(device_desc.at("enable_anisotropy")),
             .max_anisotropy = json::value_to<float>(device_desc.at("max_anisotropy")),
-            .extensions = json::value_to<std::vector<std::string>>(device_desc.at("extensions"))};
+            .extensions = detail::vector_string_to_vector_const_char(extensions)};
 }
 [[nodiscard]] json::value device_config_to_json(const device_config_s &device) {
     auto extensions = json::array(device.extensions.begin(), device.extensions.end());
@@ -34,16 +58,18 @@ namespace {
 
 } // namespace
 
-[[nodiscard]] vulkan_config_s vulkan_config_from_json(const json::value &desc) {
-    return {.layers = vulkan_layers_from_json(desc.at("layers")),
-            .extensions = json::value_to<std::vector<std::string>>(desc.at("extensions")),
-            .device = device_config_from_json(desc.at("device"))};
+[[nodiscard]] device_config_s device_config_from_json(const json::value &device_desc) {
+    auto extensions = json::value_to<std::vector<std::string>>(device_desc.at("extensions"));
+    return {.enable_anisotropy = json::value_to<bool>(device_desc.at("enable_anisotropy")),
+            .max_anisotropy = json::value_to<float>(device_desc.at("max_anisotropy")),
+            .extensions = detail::vector_string_to_vector_const_char(extensions)};
 }
-[[nodiscard]] json::value vulkan_config_to_json(const vulkan_config_s &config) {
-    auto extensions = json::array(config.extensions.begin(), config.extensions.end());
-    return {{"layers", vulkan_layers_to_json(config.layers)},
-            {"extensions", std::move(extensions)},
-            {"device", device_config_to_json(config.device)}};
+[[nodiscard]] json::value device_config_to_json(const device_config_s &device) {
+    auto extensions = json::array(detail::vector_const_char_to_vector_string(device.extensions).begin(),
+                                  detail::vector_const_char_to_vector_string(device.extensions).end());
+    return {{"enable_anisotropy", device.enable_anisotropy},
+            {"max_anisotropy", device.max_anisotropy},
+            {"extensions", std::move(extensions)}};
 }
 
 
--- src/vulkan_config.hpp
+++ src/vulkan_config.hpp
@@ -11,35 +11,24 @@
 
 namespace sm::arcane {
 
-// Note
-/*
-
-The reason for using `std::vector<std::string>` instead of `std::vector<const char*>` is that working with `boost::json`
-would require additional steps. `boost::json` does not support direct conversion from `const char*` because JSON strings
-are objects, not raw character pointers.
-
-TODO: In the future, when possible, `std::vector<std::string>` should be replaced with `std::vector<const char*>`.
-
-*/
-
 struct vulkan_layers_s {
-    std::vector<std::string> monitor_layers;
-    std::vector<std::string> validation_layers;
+    std::vector<const char *> validation_layers;
+    std::vector<const char *> monitor_layers;
 
-    BOOST_DESCRIBE_STRUCT(vulkan_layers_s, (), (monitor_layers, validation_layers))
+    BOOST_DESCRIBE_STRUCT(vulkan_layers_s, (), (validation_layers, monitor_layers))
 };
 
 struct device_config_s {
     bool enable_anisotropy;
     float max_anisotropy;
-    std::vector<std::string> extensions;
+    std::vector<const char *> extensions;
 
     BOOST_DESCRIBE_STRUCT(device_config_s, (), (enable_anisotropy, max_anisotropy, extensions))
 };
 
 struct vulkan_config_s {
     vulkan_layers_s layers;
-    std::vector<std::string> extensions;
+    std::vector<const char *> extensions;
     device_config_s device;
 
     BOOST_DESCRIBE_STRUCT(vulkan_config_s, (), (layers, extensions, device))
