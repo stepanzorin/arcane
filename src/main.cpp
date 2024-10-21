380395bd75f4af5f1df029c4b8ccb80e02a6f0c3
Stepan Zorin <stz.hom@gmail.com>
2024-10-21 23:23:15 +0300

    improved `main.cpp`

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,36 @@
 #include <cstdlib>
 #include <exception>
 
+#include <boost/system/system_error.hpp>
 #include <spdlog/spdlog.h>
 
 #include "app_config.hpp"
 #include "application.hpp"
 
-using namespace sm::arcane;
+namespace sm::arcane {
 
-int main() try {
-    Application app{app_config_from_json()};
-    app.run();
+void run(const std::shared_ptr<spdlog::logger> &global_logger) noexcept try {
+    global_logger->info("arcane is running");
 
-    return EXIT_SUCCESS;
-} catch (const std::exception &ex) {
-    spdlog::critical("The application crashed due to an unhandled exception. Reason: {}", ex.what());
-    return EXIT_FAILURE;
+    try {
+        const auto config = app_config_from_json();
+        Application app{config};
+        app.run();
+    } catch (const boost::system::system_error& ex) {
+        global_logger->critical("The application crashed due to failure config parsing. Reason: {}", ex.what());
+    }
+
+    global_logger->info("arcane successful finished");
+}  catch (const std::exception &ex) {
+    global_logger->critical("The application crashed due to an unhandled exception. Reason: {}", ex.what());
 } catch (...) {
-    spdlog::critical("The application crashed due to an unhandled exception. Reason: <unknown exception>");
-    return EXIT_FAILURE;
+    global_logger->critical("The application crashed due to an unhandled exception. Reason: <unknown exception>");
+}
+
+} // namespace sm::arcane
+
+int main() noexcept {
+    const auto global_logger = spdlog::default_logger()->clone("global");
+    sm::arcane::run(global_logger);
+    return EXIT_SUCCESS;
 }
