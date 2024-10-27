e4ae664e3780dde377a22210d3ac0fa33844b93e
Stepan Zorin <stz.hom@gmail.com>
2024-10-27 20:21:34 +0300

    removed `debug_utils.*pp`; moved implementation in `instance.cpp`

--- src/vulkan/instance.cpp
+++ src/vulkan/instance.cpp
@@ -15,11 +15,53 @@
 #include <spdlog/logger.h>
 #include <spdlog/spdlog.h>
 
-#include "debug_utils.hpp"
-
 namespace sm::arcane::vulkan {
 
 namespace {
+
+namespace detail {
+
+#if SM_ARCANE_DEBUG_MODE
+[[nodiscard]] VKAPI_ATTR VkBool32 VKAPI_CALL
+debug_utils_messenger_callback(const VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+                               const VkDebugUtilsMessageTypeFlagsEXT message_types,
+                               const VkDebugUtilsMessengerCallbackDataEXT *callback_data_ptr,
+                               void * /* user_data_ptr */) noexcept {
+    #if SM_ARCANE_DEBUG_MODE
+    // ReSharper disable once CppDefaultCaseNotHandledInSwitchStatement
+    switch (static_cast<std::uint32_t>(callback_data_ptr->messageIdNumber)) {
+        // Validation Warning: Override layer has override paths set to C:/VulkanSDK/<version>/Bin
+        case 0u: [[fallthrough]];
+
+        // Validation Warning: vkCreateInstance(): to enable extension VK_EXT_debug_utils, but this extension is
+        // intended to support use by applications when debugging, and it is strongly recommended that it be
+        // otherwise avoided.
+        case 0x822806fa: [[fallthrough]];
+
+        // Validation Performance Warning: Using debug builds of the validation layers *will* adversely affect
+        // performance.
+        case 0xe8d1a9fe: return vk::False;
+    }
+    #endif
+
+    const auto logger = spdlog::default_logger()->clone("vulkan_debug_messenger");
+    logger->error("{}", callback_data_ptr->pMessage);
+
+    return vk::False;
+}
+#endif
+
+} // namespace detail
+
+#if SM_ARCANE_DEBUG_MODE
+[[nodiscard]] vk::DebugUtilsMessengerCreateInfoEXT make_debug_utils_messenger_create_info_ext() noexcept {
+    return {{},
+            vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning | vk::DebugUtilsMessageSeverityFlagBitsEXT::eError,
+            vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
+                    vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation,
+            &detail::debug_utils_messenger_callback};
+}
+#endif
 
 [[nodiscard]] std::vector<char const *> gather_extensions(const std::shared_ptr<spdlog::logger> &vulkan_logger,
                                                           const std::vector<std::string> &instance_extensions
@@ -147,7 +189,7 @@ namespace {
 
     const auto instance_create_info = vk::StructureChain<vk::InstanceCreateInfo, vk::DebugUtilsMessengerCreateInfoEXT>{
             {{}, &application_info, enabled_layers, enabled_extensions},
-            {{}, severity_flags, message_type_flags, &debug_utils_messenger_callback}};
+            {{}, severity_flags, message_type_flags, &detail::debug_utils_messenger_callback}};
 #else
     auto instance_create_info = vk::StructureChain<vk::InstanceCreateInfo>{
             {{}, &application_info, enabled_layers, enabled_extensions}};
