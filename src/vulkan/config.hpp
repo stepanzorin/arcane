4331d1e019213da14cab169c5c89c55d3ea74467
Stepan Zorin <stz.hom@gmail.com>
2024-10-29 01:59:01 +0300

    removed old comment

--- src/vulkan/config.hpp
+++ src/vulkan/config.hpp
@@ -11,17 +11,6 @@
 
 namespace sm::arcane::vulkan {
 
-// Note
-/*
-
-The reason for using `std::vector<std::string>` instead of `std::vector<const char*>` is that working with `boost::json`
-would require additional steps. `boost::json` does not support direct conversion from `const char*` because JSON strings
-are objects, not raw character pointers.
-
-TODO: In the future, when possible, `std::vector<std::string>` should be replaced with `std::vector<const char*>`.
-
-*/
-
 struct device_config_s {
     bool enable_anisotropy;
     float max_anisotropy;
