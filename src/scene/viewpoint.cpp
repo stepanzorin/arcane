15c97a994ccf635928423cdae79463ca8a2264bb
Stepan Zorin <stz.hom@gmail.com>
2024-11-19 23:49:37 +0300

    created `viewpoint_s`

---
+++ src/scene/viewpoint.cpp
@@ -0,0 +1,54 @@
+#include "viewpoint.hpp"
+
+#include <fstream>
+#include <stdexcept>
+#include <thread>
+
+#include <boost/json.hpp>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/trigonometric.hpp>
+#include <glm/vec3.hpp>
+
+#include "util/filesystem_helpers.hpp"
+#include "util/pretty_json.hpp"
+
+namespace sm::arcane::scene {
+
+namespace json = boost::json;
+
+namespace {
+
+const auto g_viewpoint_file_path = util::application_directory_path() / "viewpoint.json";
+
+} // namespace
+
+viewpoint_s load_viewpoint_from_json() {
+    auto file = std::ifstream{g_viewpoint_file_path};
+
+    const auto content = std::string{(std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>()};
+    auto json_val = json::parse(content);
+    auto obj = json_val.as_object();
+
+    auto position = obj["position"].as_array();
+    auto orientation = obj["orientation"].as_array();
+
+    return {.position = {position[0].as_double(), position[1].as_double(), position[2].as_double()},
+            .orientation = {static_cast<float>(orientation[0].as_double()),
+                            static_cast<float>(orientation[1].as_double()),
+                            static_cast<float>(orientation[2].as_double()),
+                            static_cast<float>(orientation[3].as_double())}};
+}
+void save_viewpoint_to_json(const viewpoint_s &viewpoint) {
+    auto file = std::ofstream{g_viewpoint_file_path};
+
+    auto obj = boost::json::object{};
+    obj["position"] = json::array{{viewpoint.position.x, viewpoint.position.y, viewpoint.position.z}};
+    obj["orientation"] = json::array{
+            {viewpoint.orientation.w, viewpoint.orientation.x, viewpoint.orientation.y, viewpoint.orientation.z}};
+
+    util::write_pretty_json(file, obj);
+}
+
+} // namespace sm::arcane::scene
---
+++ src/scene/viewpoint.hpp
@@ -0,0 +1,26 @@
+// Arcane (https://github.com/stepanzorin/arcane)
+// Copyright Text: 2024 Stepan Zorin <stz.hom@gmail.com>
+
+#pragma once
+
+#include <boost/describe/class.hpp>
+#include <boost/json/fwd.hpp>
+#include <glm/fwd.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/vec3.hpp>
+
+namespace sm::arcane::scene {
+
+enum class viewpoint_action_e { base, load, save };
+
+struct viewpoint_s {
+    glm::f64vec3 position = {};
+    glm::f32quat orientation = {};
+
+    BOOST_DESCRIBE_STRUCT(viewpoint_s, (), (position, orientation))
+};
+
+[[nodiscard]] viewpoint_s load_viewpoint_from_json();
+void save_viewpoint_to_json(const viewpoint_s & /* viewpoint */);
+
+} // namespace sm::arcane::scene
